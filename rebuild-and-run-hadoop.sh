f90794173036e3278e8d9cfa892bd9a6a3cc845d
touhiDroid <tz08128@gmail.com>
2021-11-16 05:19:37 -0500

    sh-script added with maven clean-package & hadoop run cmds, gitignore added

---
+++ rebuild-and-run-hadoop.sh
@@ -0,0 +1,5 @@
+source ~/.bashrc
+mvn clean -f pom.xml
+mvn package -f pom.xml
+rm -R knnOut
+hadoop jar target/2021-603-A3-Touhiduzzaman-1.0.0.jar com.touhiDroid.Knn datasets/small-train.arff datasets/small-test.arff knnOut
